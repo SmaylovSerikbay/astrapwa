3eb8d4478d55c6f6065406bbd889356f34a91384
SmaylovSerikbay <augmented.vr@gmail.com>
2026-01-21 23:13:23 +0500

    Refactor code structure for improved readability and maintainability

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"871f65ac1bf129edb222c3293a636ff4b67534
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2873916040"
+    serviceWorkerVersion: "3437177489"
   }
 });
