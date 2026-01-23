5449f73eb57118415af0edc35ae709863e57bb86
SmaylovSerikbay <augmented.vr@gmail.com>
2026-01-23 12:37:52 +0500

    Update service worker version in flutter_bootstrap.js; adjust resource hashes in flutter_service_worker.js and main.dart.js

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"871f65ac1bf129edb222c3293a636ff4b67534
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "187173564"
+    serviceWorkerVersion: "3206832385"
   }
 });
